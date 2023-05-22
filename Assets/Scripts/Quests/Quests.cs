c47ee9f5fd4e9dec70cb6a4976e70ce70dd204ff
AIE\s213300 <s213300@students.aie.edu.au>
2023-05-23 09:23:44 +0930

    Removed old quest script, replaced with new Quest and QuestGiver scripts

--- Assets/Scripts/Quests/Quests.cs
+++ Assets/Scripts/Quests/Quests.cs
@@ -1,50 +1,18 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
 public class Quest
 {
-    public string questName;
-    // Add other quest properties as needed
-}
+	public bool isActive;
 
-public class QuestManager : MonoBehaviour
-{
-    public List<Quest> quests = new List<Quest>();
 
-    public static object Instance { get; internal set; }
+   public string title;
+   public string description;
+   public int experienceReward
+   public int goldReward;
 
-    public void ShowQuests()
-    {
-        Debug.Log("Quests:");
-
-        foreach (Quest quest in quests)
-        {
-            Debug.Log("Quest Name: " + quest.questName);
-            // Display other quest information if needed
-        }
-    }
-
-    public void AddQuest(string questName)
-    {
-        Quest newQuest = new Quest();
-        newQuest.questName = questName;
-        quests.Add(newQuest);
-
-        Debug.Log("Added quest: " + questName);
-    }
-
-    public void RemoveQuest(string questName)
-    {
-        Quest questToRemove = quests.Find(quest => quest.questName == questName);
-
-        if (questToRemove != null)
-        {
-            quests.Remove(questToRemove);
-            Debug.Log("Removed quest: " + questName);
-        }
-        else
-        {
-            Debug.Log("Quest not found: " + questName);
-        }
-    }
+  
 }
